d49514b15e5fe0b5e54a36e5cd6ee5491db1b5f9
Your Name <your.email@example.com>
2026-05-01 23:31:40 +0530

    Fix: Switched to ultra-light HuggingFace model to prevent Render OOM

--- travel_tools/vectorstore_provider.py
+++ travel_tools/vectorstore_provider.py
@@ -3,13 +3,13 @@ from langchain_chroma import Chroma
 from langchain_classic.chains.query_constructor.base import AttributeInfo
 from langchain_classic.retrievers import SelfQueryRetriever
 from langchain_groq import ChatGroq
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 
 from .config import CHROMA_DB_PATH
 
 load_dotenv()
 
-embedding_func = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+embedding_func = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
 vectorstore = None
 retriever = None
 
